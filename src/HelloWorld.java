8109c406c6b35cd566ea6874a47daaf47214f834
bluebaka <cedric_georgi@hotmail.com>
2016-04-05 17:01:12 +0200

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld
 	public static void main (String [] args)
 	{
 		System.out.println("good morniinnnnnnnnnnnnng Vietnammmmmmmm");
+		System.out.println("Bonjourrrrrrrr PARIS");
 	}
 }
